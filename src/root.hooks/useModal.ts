dfc048ef40f8443f3b3ba90f9070d5bef7281976
ghoon99 <ghoon99@kakao.com>
2022-10-31 17:57:50 +0900

    chore: useModal 커스텀 훅 주석 추가

--- src/root.hooks/useModal.ts
+++ src/root.hooks/useModal.ts
@@ -2,6 +2,18 @@ import { useState } from "react";
 
 type UseModalHookResult = [boolean, () => void, () => void];
 
+/**
+ * ### useModal 커스텀 훅 
+ * 
+ * 모달을 열고 닫는 핸들러 함수, 모달의 열림 상태를 반환하는 커스텀 훅입니다.
+ * 
+ * @example ```tsx
+ *  const [isOpen, openModal, closeModal] = useModal();
+ *  ...
+ *  <Button onClick={openModal}>모달 오픈</Button>
+      {isOpen && <Modal onClose={closeModal} />
+ * ``` 
+ */
 export const useModal = (): UseModalHookResult => {
   const [isOpen, setIsOpen] = useState(false);
   const openModal = () => setIsOpen(true);
