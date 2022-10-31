57329680ff3a74e3018bdb57f8be54ca5f05f519
ghoon99 <ghoon99@kakao.com>
2022-10-31 18:54:05 +0900

    feat: 토스트 메시지 설정,  useToast hook 반환값 수정

---
+++ src/root.features/auth/libs/constants/ToastMessages.ts
@@ -0,0 +1,2 @@
+export const LOGIN_SUCCESS_MESSAGE = "✅ 로그인에 성공했습니다";
+export const LOGIN_ERROR_MESSAGE = "⚠️ 로그인에 실패했습니다. 다시시도해주세요";
---
+++ src/root.libs/constants/ErrorMessages.ts
@@ -0,0 +1,2 @@
+export const UNKNOWN_ERROR_MESSAGE =
+  "⚠️ 알 수 없는 오류가 발생했습니다. 다시시도해주세요";
